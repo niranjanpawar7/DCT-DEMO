3e2ad135c35c02065af9c60c714be9adad3cf969
Niranjan Pawar <pkumar@prohaktiv.com>
2019-02-27 19:16:13 +0530

    Added Calender View

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
-import { TrainingDetailComponent } from './training-detail/training-detail.component';
+import { TrainingDetailComponent } from './training-detail/training-detail.component'; 
 
 
 @NgModule({
--- src/app/calender/calender.module.ts
+++ src/app/calender/calender.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common"; 
+import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { CalenderMaterialModule } from './calender-material.module';
-import { calenderRoutingModule } from './routing-calender.module';  
+import { calenderRoutingModule } from './routing-calender.module';
 import { CalenderComponent, DialogContentExampleDialog } from './calender.component';
+import { ViewCalenderComponent } from '../view-calender/view-calender.component';
+import { FullCalendarModule } from 'ng-fullcalendar';
 
-//import { EventDetailComponent } from '../event-detail/event-detail.component';
-//import { ExpensesComponent } from '../expenses/expenses.component';
- 
- 
+
 
 @NgModule({
   imports: [
-      CommonModule,
-      RouterModule, 
-      calenderRoutingModule,
-      CalenderMaterialModule 
-    ],
+    CommonModule,
+    RouterModule,
+    calenderRoutingModule,
+    CalenderMaterialModule,
+    FullCalendarModule
+  ],
   declarations: [
     CalenderComponent,
-    DialogContentExampleDialog
-    //ExpensesComponent, 
+    DialogContentExampleDialog,
+    ViewCalenderComponent
   ],
   entryComponents: [DialogContentExampleDialog]
 })
 
-export class CalenderModule {}
+export class CalenderModule { }
