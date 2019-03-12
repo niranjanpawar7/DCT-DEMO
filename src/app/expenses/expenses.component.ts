c692ceb3030352efddb328a860ed4bc133a744d4
Niranjan Pawar <pkumar@prohaktiv.com>
2019-03-12 15:46:12 +0530

    Added Fonts

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -28,9 +28,9 @@ export class ExpensesComponent implements OnInit {
   dataSource = ELEMENT_DATA;
 
   foods = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+    // {value: 'steak-0', viewValue: 'Steak'},
+    // {value: 'pizza-1', viewValue: 'Pizza'},
+    // {value: 'tacos-2', viewValue: 'Tacos'}
   ];
 
   isLinear = false;
