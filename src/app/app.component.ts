67dd979019e5b230238292635920868a426f06f6
Niranjan Pawar <pkumar@prohaktiv.com>
2019-03-28 11:48:44 +0530

    added nab close by defaul code in responnsivness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Front-End';
-  public show_dialog : boolean = false;
+  //public show_dialog : boolean = false;
   public button_name : any = 'Show Login Form!';
 
 
-  toggle() { 
-    this.show_dialog = !this.show_dialog; 
-    // CHANGE THE TEXT OF THE BUTTON.
-    if(this.show_dialog) 
-      this.button_name = "Hide Login Form!";
-    else
-      this.button_name = "Show Login Form!";
-  }
+  // toggle() { 
+  //   this.show_dialog = !this.show_dialog; 
+  //   // CHANGE THE TEXT OF THE BUTTON.
+  //   if(this.show_dialog) 
+  //     this.button_name = "Hide Login Form!";
+  //   else
+  //     this.button_name = "Show Login Form!";
+  // }
 }
+
